fc5b72ee6d4d2418a5b176686d21fd2e7a5edbb2
joeyb <joeybesseling@icloud.com>
2023-11-11 14:17:57 +0100

    temp commit

--- code/utils/constants.py
+++ code/utils/constants.py
@@ -1,17 +1,17 @@
-DATA_BASE_PATH = '../data/m5-forecasting-accuracy/'
+DATA_BASE_PATH: str = '../data/m5-forecasting-accuracy/'
 DATA_BASE_PATH_UNCERTAINTY = '../data/m5-forecasting-uncertainty/'
-SALES_EVALUATION = 'sales_train_evaluation.csv'
-SALES_VALIDATION = 'sales_train_validation.csv'
-CALENDAR = 'calendar.csv'
-SAMPLE_SUBMISSION = 'sample_submission.csv'
-SELL_PRICES = 'sell_prices.csv'
+SALES_EVALUATION: str = 'sales_train_evaluation.csv'
+SALES_VALIDATION: str = 'sales_train_validation.csv'
+CALENDAR: str = 'calendar.csv'
+SAMPLE_SUBMISSION: str = 'sample_submission.csv'
+SELL_PRICES: str = 'sell_prices.csv'
 
-PRECOMPUTED_BASE_PATH = '../data/uncertainty/features/'
+PRECOMPUTED_BASE_PATH: str = '../data/uncertainty/features/'
 
 DAYS: int = 28
 QUANTILES: int = [0.005, 0.025, 0.165, 0.25, 0.50, 0.75, 0.835, 0.975, 0.995]
 
-AGG_LEVEL_COLUMNS = {
+AGG_LEVEL_COLUMNS: dict = {
     "Level1": [], # no grouping, sum of all
     "Level2": ['state_id'],
     "Level3": ['store_id'],
@@ -28,12 +28,12 @@ AGG_LEVEL_COLUMNS = {
 
 D_CROSS_VAL_START_LIST = [1802, 1830, 1858, 1886, 1914]
 
-TEST_PATH = 'test/'
-PREDICTION_BASE_PATH = '../data/uncertainty/temp_submissions/'
-SUBMISSION_BASE_PATH = '../data/uncertainty/final_submissions/'
+TEST_PATH: str= 'test/'
+PREDICTION_BASE_PATH: str = '../data/uncertainty/temp_submissions/'
+SUBMISSION_BASE_PATH: str = '../data/uncertainty/final_submissions/'
 
 SUB_D_START_VAL: int = 1914
-SUB_D_START_EVAL: int = 1914 + 28
+SUB_D_START_EVAL: int = 1914 + DAYS
 
 # the columns are always included after feature processing
 # because they are required in the training and submission format
