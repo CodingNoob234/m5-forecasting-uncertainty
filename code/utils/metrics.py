12940678533187d5f1e74c58553dc29544a90510
joeyb <joeybesseling@icloud.com>
2023-12-23 01:37:14 +0100

    function for Diebold Mariano for quantile regression, not working yet

--- code/utils/metrics.py
+++ code/utils/metrics.py
@@ -125,3 +125,71 @@ def SPL(df_pred, df_true, df_true_hist: pd.DataFrame):
         for q in QUANTILES
     ]
     return np.mean(all_quantiles) / scale
+
+########################### DIEBOLD MARIANO ###########################
+def DM_test_pinball(pred1: pd.DataFrame, pred2: pd.DataFrame, true: pd.DataFrame, h):
+    df = pd.merge(
+        pred1,
+        pred2,
+        how = 'inner',
+        on = ['quantile', 'd', 'agg_column1', 'agg_column2']
+    )
+    df = pd.merge(
+        df,
+        true,
+        on = ['d', 'agg_column1', 'agg_column2'],
+        how = 'left'
+    )
+    
+    resid = df['sold'] - df['pred']
+    quantile = df['quantile']
+    idx = resid >= 0
+    pinball_resid = resid
+    pinball_resid[idx] = resid[idx] * (1 - quantile[idx])
+    pinball_resid[~idx] = resid[~idx] * quantile[idx]
+    df['pinball_resid'] = pinball_resid
+    
+    df_qtile_avg = df.groupby(['d', 'agg_column1', 'agg_column2'])['pinball_resid']\
+        .mean().reset_index(drop=False)
+
+    ids = []
+    stats = []
+    p_values = []
+    for (agg_column1, agg_column2), df_s in df_qtile_avg.groupby(['agg_column1', 'agg_column2']):
+        
+        # compute cov
+        p_s = df_s['pinball_resid']
+        mean = p_s.mean()
+        T = len(p_s)
+        
+        def auto_cov(resid, lag, mean):
+            cov = 0
+            T = float(len(resid))
+            for i in np.arange(0, len(resid)-lag):
+                autoCov += ((resid[i+lag])-mean)*(resid[i]-mean)
+            return (1/(T))*autoCov
+        
+        gamma = []
+        for lag in range(h):
+            gamma.append(auto_cov(p_s, lag, mean))
+        
+        # compute stat
+        V_d = (gamma[0] + 2*sum(gamma[1:]))/T
+        DM_stat=V_d**(-0.5)*mean
+        harvey_adj=((T+1-2*h+h*(h-1)/T)/T)**(0.5)
+        DM_stat = harvey_adj*DM_stat
+        
+        # compute p_value
+        from scipy.stats import t
+        p_value = 2*t.cdf(-abs(DM_stat), df = T - 1)
+        
+        # store results
+        ids.append(agg_column1 + '_' + agg_column2)
+        stats.append(DM_stat)
+        p_values.append(p_value)
+        
+    return pd.DataFrame({
+        'ids': ids,
+        'stats': stats,
+        'p_values': p_values
+    })
